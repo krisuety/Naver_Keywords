e29681014c0e8618f38c02fb92399e433f93a66d
Ubuntu <ubuntu@ip-172-31-29-33.ap-northeast-2.compute.internal>
2019-01-10 12:53:45 +0900

    modified find_variable.py change article path

--- find_variable.py
+++ find_variable.py
@@ -5,12 +5,13 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
 import pandas as pd
 import pickle
-import requests, json, sys
+import requests, json, sys, os
 
 def fine_variable(alpha):
-
+    path = os.path.dirname(os.path.realpath(__file__))
     # 기사 데이터 프레임 로드
-    article_df = pd.read_pickle("article_2016-06-01.plk")
+    print(path)
+    article_df = pd.read_pickle("{}/article_2016-06-01.plk".format(path))
 
     # 테스트 데이터와 트레인 데이터 분리
     X_train, X_test, y_train, y_test = train_test_split(article_df.content, article_df.category, test_size=0.1, random_state=1)
